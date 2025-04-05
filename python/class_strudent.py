df1b0202476b5f381c85b0e80a2919d48e413ec3
freshmea <freshmea@naver.com>
2025-04-05 14:52:33 +0900

    학생 클래스 추가 및 인스턴스 생성 예제 구현

--- python/class_strudent.py
+++ python/class_strudent.py
@@ -1,5 +1,23 @@
+class Student:
+    def __init__(self, name, korean, math, english, science, *a, **kwargs):
+        self.name = name # a.name self -> a
+        self.korean = korean
+        self.math = math
+        self.english = english
+        self.science = science
+        # (*this) = self
+
+
 def main():
-    pass
+    a = Student("choi", 10, 20, 15, 17)
+    b = Student("pack", 20, 20 ,18, 17)
+    print(a, b)
+    print(a.name)
+    print(a.korean)
+    print(a.science)
+    print(b.name)
+    print(b.korean)
+    print(b.science)
 
 if __name__ == "__main__":
     main()
