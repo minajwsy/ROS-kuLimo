e199e8bf2232f6ed0c9fbe8dcfe6918000cdab64
freshmea <freshmea@naver.com>
2025-04-05 15:34:43 +0900

    학생 클래스에 총점 및 평균 계산 메서드 추가, 졸업생 클래스 구현

--- python/class_strudent.py
+++ python/class_strudent.py
@@ -1,11 +1,22 @@
 class Student:
     def __init__(self, name, korean, math, english, science, *a, **kwargs):
+        # (*this) = self
         self.name = name # a.name self -> a
         self.korean = korean
         self.math = math
         self.english = english
         self.science = science
-        # (*this) = self
+        self.sum = 0
+
+    def get_sum(self):
+        sum = self.korean + self.math + self.english + self.science
+        return sum
+
+    def get_calculated_sum(self):
+        return self.sum
+
+    def get_average(self):
+        return (self.korean + self.math + self.english + self.science) / 4
 
 
 def main():
@@ -18,6 +29,9 @@ def main():
     print(b.name)
     print(b.korean)
     print(b.science)
+    # print(a.get_sum())
+    print(a.get_calculated_sum())
+    print(a.get_average())
 
 if __name__ == "__main__":
     main()
