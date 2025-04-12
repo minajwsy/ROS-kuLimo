05c211b3e9cf85c280923e4a08223ef94f2aef35
freshmea <freshmea@naver.com>
2025-04-12 06:16:08 +0000

    hello topic

--- catkin_ws/src/hello_ros/script/hello.py
+++ catkin_ws/src/hello_ros/script/hello.py
@@ -1,6 +1,19 @@
 # catkin_make
+import rospy
+from std_msgs.msg import String
+
+
 def main():
-    print("hello, ROS1 noetic!!")
+    rospy.init_node('hello', anonymous=True)
+    pub = rospy.Publisher('message', String, queue_size=10)
+    data = String()
+    i=0
+    data.data = f"hellom, ROS! netic {i}"
+    rate = rospy.Rate(3)
+    while True:
+        pub.publish(data)
+        print("hello, ROS1 noetic!!")
+        rate.sleep()
 
 if __name__ == "__main__":
     main()
