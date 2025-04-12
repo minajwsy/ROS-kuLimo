9a8facb1c19a3702389aaa3cf0c453d356b9887a
freshmea <freshmea@naver.com>
2025-04-12 06:50:25 +0000

    aa

--- catkin_ws/src/hello_ros/script/moveTurtle.py
+++ catkin_ws/src/hello_ros/script/moveTurtle.py
@@ -10,11 +10,15 @@ def main():
     rospy.init_node('hello', anonymous=True)
     pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
     data = Twist()
-    data.angular.z = 1.0
-    data.linear.x = 2.0
+    data.angular.z = 3.0
+    velocity = 0.0
 
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
+        data.linear.x = velocity
+        velocity += 0.1
+        if velocity > 10:
+            velocity = 0
         pub.publish(data)
         rate.sleep()
 
